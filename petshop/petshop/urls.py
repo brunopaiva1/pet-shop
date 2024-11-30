787293817dc9b053ad3fec0280b0ac15d1bec460
TiagoDev23 <tiagojones11@gmail.com>
2024-11-30 18:18:51 -0300

    Criação do crud relacionado ao cliente

--- petshop/petshop/urls.py
+++ petshop/petshop/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+from rest_framework.authtoken import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from cliente.api.views import ClienteViewSet
+
+router = SimpleRouter()
+
+router.register("Cliente", ClienteViewSet, basename="Clientes")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path("admin/", admin.site.urls),
+    path("api/token-auth/", views.obtain_auth_token),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+]+router.urls
