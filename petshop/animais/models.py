d7ed7fdb4b2b4ebddbfd2086974b3f470ad81c93
TiagoDev23 <tiagojones11@gmail.com>
2024-11-30 18:36:50 -0300

    Criação do crud relacionado a entidade animal

--- petshop/animais/models.py
+++ petshop/animais/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from cliente.models import Cliente
+
 # Create your models here.
+
+class Animal(models.Model):
+    nome = models.CharField(max_length=100)
+    raca = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    peso = models.FloatField()
+    dono = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    
+def __str__(self):
+        return f'Animal {self.nome}'
+    
+class Meta:
+    verbose_name = "Animal"
+    verbose_name_plural = "Animais"    
