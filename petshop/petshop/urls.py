d1e989c4a7334c48f7c6e02aa6a7e9bcfbcf445c
brunopaiva1 <brunopaivamaster10@gmail.com>
2024-11-30 23:45:46 -0300

    Mensagem explicando as alterações

--- petshop/petshop/urls.py
+++ petshop/petshop/urls.py
@@ -21,7 +21,9 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-    
+    openapi.Info(
+        title="API do PetShop",
+    )
 )
 
 urlpatterns = [
